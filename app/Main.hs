9fb8e786f365f8ce5e99bd9584ae7539dee44eed
Marc Jakobi <marc.jakobi@velasolaris.com>
2021-09-22 22:46:05 +0200

    Add profiling simulator

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,23 @@
 module Main (main) where
 
+import Prelude
+import Text.XML.Prettify
+import qualified Data.Text.IO as TIO
+import Control.DeepSeq
+
 
 main :: IO ()
-main = putStrLn "App not implemented yet."
+main = profileSimulate
+
+-- Simulates repeated uses for profiling
+profileSimulate :: IO ()
+profileSimulate = do
+  uglyXml <- TIO.readFile uglyXmlFile
+  let uglyXmlList = replicate nRuns uglyXml
+  let prettyXmlList = map prettyPrintXml uglyXmlList
+  pure (deepseq prettyXmlList "Done") >>= putStrLn
+  where
+    uglyXmlFile :: FilePath
+    uglyXmlFile = "data/tests/prettyPrintTest.xml"
+    nRuns = 10000
+
